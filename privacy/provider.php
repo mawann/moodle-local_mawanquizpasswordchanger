24952981b5a9940af1abdb7385431cd460eb8d73
mawann <mawan911@yahoo.com>
2025-01-31 22:01:08 +0700

    Update.

--- privacy/provider.php
+++ privacy/provider.php
@@ -15,6 +15,8 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 //
 // @package local_mawanquizpasswordchanger
+// @copyright 2025 Mawan Agus Nugroho
+// @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
 
 // Prevent direct access to this script.
 defined('MOODLE_INTERNAL') || die();
