df8bc897a8eed0cf6f856e55759fd5761133501f
mawann <mawan911@yahoo.com>
2025-01-31 23:23:28 +0700

    Update.

--- classes/task/send_data.php
+++ classes/task/send_data.php
@@ -14,6 +14,8 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
+require_once(__DIR__ . '/../../config.php');
+
 namespace local_mawanquizpasswordchanger\task;
 
 /**
